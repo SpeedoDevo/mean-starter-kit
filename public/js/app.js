9b2fff669446cb4e9cc510ae562ea9b8d411f0c9
SpeedoDevo <speedo@devo.hu>
2014-08-07 15:38:11 +0100

    done

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+angular.module('meanStarter', ['ngRoute', 'Routes', 'Controllers', 'Services', 'Directives']);
